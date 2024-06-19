b16439e08974dc03ca289d3f74f4ceac6f9ccea4
czc <2940996969@qq.com>
2024-06-19 21:40:36 +0800

    InfiniteFly

---
+++ config.py
@@ -0,0 +1,21 @@
+import pygame
+
+version = "V2.0"
+RUN = True
+SCREEN_WIDTH = 800  # 宽度
+SCREEN_HEIGHT = 500  # 高度
+BG_COLOR = pygame.Color(0, 0, 0)  # 颜色
+TEXT_COLOR = pygame.Color(255, 0, 0)  # 字体颜色
+O_COLOR = pygame.Color(255, 255, 255)
+scaled_width = 60
+scaled_height = 60
+
+window = None
+myplance = None
+enemyList = []  # 敌方坦克列表
+enemyCount = 5  # 敌方坦克数量
+myBulletList = []  # 我方坦克子弹列表
+enemyBulletList = []  # 敌方坦克子弹列表
+explodeList = []  # 爆炸效果列表
+WallList = []  # 墙壁列表
+hitTank = 0
